81154b81131ab924f41ea2259c7c03c63fbda365
Mohamed Sameer <dr.msameer@gmail.com>
2025-10-24 14:57:18 +0900

    mohamed commit =some changes commit

--- Code_Examples/001_Chapter2/06_convert.cpp
+++ Code_Examples/001_Chapter2/06_convert.cpp
@@ -1,6 +1,8 @@
 // convert.cpp -- converts stone to pounds
 #include <iostream>
+
 int stonetolb(int);     // function prototype
+
 int main()
 {
     using namespace std;
@@ -19,3 +21,11 @@ int stonetolb(int sts)
 {
      return 14 * sts;
 }
+
+
+/*
+Here’s a sample run of the program in Listing 2.6:
+Enter the weight in stone: 15
+15 stone = 210 pounds.
+
+*/
